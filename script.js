90418e2b3a92c664371c92bcc5ab64b16c606e42
myusername <[madanbandiatmakur@gmail.com]>
2025-03-13 23:28:17 +0530

    Fix the corrections

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const works = {
-    "task1": { "title": "Project 1", "url": "task1/index.html" },
-    "task2": { "title": "Project 2", "url": "task2/index.html" },
-    "task3": { "title": "Project 3", "url": "task3/index.html" }
+    "task1": { "title": "Project 1", "url": "works/task1/index.html" },
+    "task2": { "title": "Project 2", "url": "works/task2/index.html" },
+    "task3": { "title": "Project 3", "url": "works/task3/index.html" }
 };
 
 function loadNavigation() {
